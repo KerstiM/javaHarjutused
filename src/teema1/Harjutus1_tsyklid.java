a7a02de9350abf2176e21dc13bfe396db08df6bb
KerstiM <kmiller@itcollege.ee>
2015-10-24 18:53:25 +0300

    -harjutus 1

--- src/teema1/Harjutus1_tsyklid.java
+++ src/teema1/Harjutus1_tsyklid.java
@@ -1,5 +1,8 @@
 package teema1;
 
+        import java.util.ArrayList;
+        import java.util.Arrays;
+
 /**
  * Kasutades tsükleid, lahendada järgmised ülesanded:
  * 1. Trükkida ekraanile numbrid 10 kuni 1
@@ -19,5 +22,46 @@ package teema1;
 public class Harjutus1_tsyklid {
     public static void main(String[] args) {
 
+        /**int  l = 10; //Print 10 - 1 v1
+         while (l > 0) {
+         System.out.print(l + " ");
+         l = l - 1;
+         }
+         */
+
+        for (int l = 10; l>0; l--) { //Print 10 - 1 v2
+            System.out.println(l + " ");
+        }
+
+        System.out.print("\n");
+        for (int l = 1; l<=10; l=l+2) {  //Print paaritud v1
+            System.out.print(l + " ");
+        }
+
+        System.out.print("\n");
+        for (int l = 0; l<5; l++) { //Print paaritud v2
+            System.out.print(2*l+1 + " ");
+        }
+
+        System.out.print("\n");
+        System.out.print("\n");
+
+
+        for (int i = 0; i < 10; i++) { //annab rea
+            for (int a = 0; a < 10; a++) { //annab veeru
+                int tabel = i + a; // tekitab tabeli
+                if (tabel >= 10) {  //kui number on üle 10,ne siis lahtuab 10, jõuab tagasi algusesse
+                    tabel = tabel - 10;
+                }
+                System.out.print(tabel + " "); //prindib tabeli ja paneb numbrite vahele tühikud
+            }
+            System.out.println(); //prindib iga rea eraldi reale
+        }
     }
 }
+
+
+
+
+
+
