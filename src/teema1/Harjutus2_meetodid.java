2854d6a9c504f5ad23ab0ca47e93fb76d82ac93a
KerstiM <kmiller@itcollege.ee>
2015-10-24 22:42:29 +0300

    -harjutus 2

--- src/teema1/Harjutus2_meetodid.java
+++ src/teema1/Harjutus2_meetodid.java
@@ -1,5 +1,6 @@
 package teema1;
 
+import java.util.Scanner;
 /**
  * 1. Kirjutada meetod, mis tagastab parameetrina etteantud arvu kuubi.
  * 2. Kirjutada meetod, mis saab parameetritena kaks arvu (vahemiku) ning
@@ -10,13 +11,41 @@ package teema1;
 public class Harjutus2_meetodid {
 
     public static void main(String[] args) {
+
+     //1. ülesanne (kasutaja sisestus: http://www.homeandlearn.co.uk/java/user_input.html)
+
+        /**Scanner kasutajaSisestus = new Scanner(System.in); //kasutaja sisestuse registreerib scanner. Selleks import java.util.Scanner; tehakse täpselt class´i üles.
+
+        int arv;
+        System.out.println("Sisesta täisarv: "); //kasutajal palutakse sisetada arv
+        arv = kasutajaSisestus.nextInt(); //next aitab kasutaja sisestust saada. kasutaja sisestus muudetakse intiks(teksti puhul stringiks)
+        System.out.println(tostaKuupi(arv)); //tagastatakse arvu kuup, mis on arvutatud tostaKuupi
+    }*/
+        kasutajaSisestus("Sisesta täisarv:", 1, 99); //vahemik ???
+    }
+    public static void kasutajaSisestus(String kysimus, int min, int max) {
+
+        boolean arvVahemikus = false;
+        while (!arvVahemikus) {
+            Scanner sisend = new Scanner(System.in);
+
+            System.out.println(kysimus);
+            int arv = sisend.nextInt();
+            if (arv <= min) {
+                System.out.println("Sisesta suurem arv");
+            }
+            else {
+                if (arv >= max) {
+                    System.out.println("Sisesta väiksem arv");
+                } else {
+                        System.out.println("Oled vahemikus");
+                        arvVahemikus = true;
+                }
+            }
+        }
     }
 
-    public static int tostaKuupi(int arv) {
-        return 0;
-    }
-
-    public static int kasutajaSisestus(String kysimus, int min, int max) {
-        return 0;
+    private static int tostaKuupi(int arv) {
+        return (int) Math.pow(arv, 3);
     }
 }
